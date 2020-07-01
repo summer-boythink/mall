1b011cb1534a791e6b2e2cf6987de166d7087e93
summer-boythink <2071792205@qq.com>
2020-07-01 14:52:47 +0800

    tang

---
+++ src/network/request.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+export function request(config){
+ 
+const instance1 = axios.create({
+    baseURL:'http://123.207.32.32:8000'
+})
+
+//axios拦截器
+instance1.interceptors.request.use(config => {
+    console.log(config);
+    return config;
+},err => {
+
+})
+
+instance1.interceptors.response.use(res => {
+    return res.data
+},err => {
+
+})
+
+return instance1(config)   //返回promis，所以可以在实例后面直接then
+}
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,13 +1,36 @@
 <template>
-    <h2>is 主页</h2>
+<div id="home">
+    <navbar class="home-nav"><div slot="center">购物</div></navbar>
+</div>
 </template>
 
 <script>
+import navbar from 'components/common/navbar/navbar'
+import {gethomemultidata} from 'network/home'
 export default {
-    name:"home"
+    name:"home",
+    components:{
+        navbar
+    },
+    data(){
+        return {
+            banners:[],
+            recommends:[]
+        }
+    },
+    created(){
+        gethomemultidata().then(res => {
+            console.log(res);
+            this.banners = res.data.banner.list
+            this.recommends = res.data.recommend.list
+        })
+    }
 }
 </script>
 
 <style scoped>
-
+    .home-nav{
+        background-color:pink;
+        color:#fff
+    }
 </style>
